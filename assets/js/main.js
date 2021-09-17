887fbe0553fa5cbba109ca7d0e1072f89c650bc6
tuanbuinov09 <86548995+tuanbuinov09@users.noreply.github.com>
2021-09-17 17:31:55 +0700

    change

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+const swiper = new Swiper('.swiper.swiper-1', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  autoplay: {
+    delay: 5000,
+  },
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true
+  },
+});
+const swiper2 = new Swiper('.swiper.swiper-2', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
