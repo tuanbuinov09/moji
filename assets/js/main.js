d0e24226714f8bcb6bac834c695aaa369fa56576
tuanbuinov09 <86548995+tuanbuinov09@users.noreply.github.com>
2021-09-30 10:24:17 +0700

    responsive 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,15 +49,22 @@ const swiper2 = new Swiper('.swiper.swiper-2', {
 document.querySelector(".mobile-menu-button").addEventListener("click", function (e) {
   if (this.classList.contains("active")) {
     this.classList.remove("active");
+    document.querySelector(".nav__bottom").classList.remove("active");
+    document.querySelector(".overcoat").classList.remove("active");
+    return;
   } else {
     this.classList.add("active");
+    document.querySelector(".nav__bottom").classList.add("active");
+    document.querySelector(".overcoat").classList.add("active");
   }
 });
 
 window.onscroll = function (e) {
   var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-  console.log(scrollTop)
   if (scrollTop > 73) {
+    if(document.querySelector(".nav__top").style.top === "-32px"){
+      return;
+    }
     document.querySelector(".nav__middle-left").style.boxShadow = "0px 0px 4px -2px"
     document.querySelector(".nav__top").style.transition = "all .3s ease-out";
     document.querySelector(".nav__top").style.top = "-32px";
@@ -67,7 +74,12 @@ window.onscroll = function (e) {
     document.querySelector(".nav__log-in").style.top = 0;
     document.querySelector(".nav__bottom").style.transition = "all .3s ease-out";
     document.querySelector(".nav__bottom").style.top = 105-34.4 +"px";
+    document.querySelector(".mobile-menu-button").style.transition = "all .3s ease-out";
+    document.querySelector(".mobile-menu-button").style.top = "6px";
   } else {
+    if(document.querySelector(".nav__top").style.top === "0px"){
+      return;
+    }
     document.querySelector(".nav__middle-left").style.boxShadow = "unset";
     document.querySelector(".nav__top").style.transition = "all .3s ease-out";
     document.querySelector(".nav__top").style.top = "0px";
@@ -77,5 +89,18 @@ window.onscroll = function (e) {
     document.querySelector(".nav__log-in").style.top = "32px";
     document.querySelector(".nav__bottom").style.transition = "all .3s ease-out";
     document.querySelector(".nav__bottom").style.top = "105px";
+    document.querySelector(".mobile-menu-button").style.transition = "all .3s ease-out";
+    document.querySelector(".mobile-menu-button").style.top = "38px";
   }
+}
+var submenuContainers = document.querySelectorAll(".nav__submenu-container");
+for(var i=0; i< submenuContainers.length; i++){
+    submenuContainers[i].addEventListener("click", function(e){
+        if(this.querySelector(".submenu").classList.contains("active")){
+            this.querySelector(".submenu").classList.remove("active");
+            return;
+        }else{
+          this.querySelector(".submenu").classList.add("active");
+        }
+    })
 }
